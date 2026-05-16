e7682066e1715cc7a4d58a3da8d55bb4b186e657
Jungea <jungea2241@gmail.com>
2026-05-16 18:41:31 +0900

    MainScene: 사이드뷰 배경 (벽, 바닥)

--- src/game/scenes/MainScene.ts
+++ src/game/scenes/MainScene.ts
@@ -1,7 +1,38 @@
 import Phaser from 'phaser'
 
+const FLOOR_Y_RATIO = 0.65
+
 export class MainScene extends Phaser.Scene {
+  private wallGraphics!: Phaser.GameObjects.Graphics
+  private floorGraphics!: Phaser.GameObjects.Graphics
+
   constructor() { super('MainScene') }
-  create() {}
+
+  create() {
+    this.drawBackground()
+  }
+
+  private drawBackground() {
+    const { width, height } = this.cameras.main
+    const floorY = height * FLOOR_Y_RATIO
+
+    this.wallGraphics?.destroy()
+    this.floorGraphics?.destroy()
+
+    // 벽
+    this.wallGraphics = this.add.graphics()
+    this.wallGraphics.fillStyle(0x2a2240)
+    this.wallGraphics.fillRect(0, 0, width, floorY)
+
+    // 바닥
+    this.floorGraphics = this.add.graphics()
+    this.floorGraphics.fillStyle(0x3d2b1a)
+    this.floorGraphics.fillRect(0, floorY, width, height - floorY)
+
+    // 바닥선
+    this.floorGraphics.lineStyle(2, 0x6b4f3a)
+    this.floorGraphics.lineBetween(0, floorY, width, floorY)
+  }
+
   update() {}
 }
